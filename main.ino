5bca80e7748c9a6c08ae77e7f78b0500138178f6
koki0517 <45210810@gse.okayama-c.ed.jp>
2022-09-04 23:30:08 +0900

    rename

--- main.ino
+++ main.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
@@ -34,4 +35,5 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  Serial.println("Hello World");
 }
