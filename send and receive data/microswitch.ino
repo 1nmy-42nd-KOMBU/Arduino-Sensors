c497171337e7e42f82aa4cc8b828a2d0f3df4d07
koki0517 <45210810@gse.okayama-c.ed.jp>
2022-09-13 00:28:45 +0900

    ultrasonic sensor

--- send and receive data/microswitch.ino
+++ send and receive data/microswitch.ino
@@ -1,9 +1,5 @@
 #include<Wire.h> // I2C library
 
-const int pingPin = 2;
-unsigned long duration;
-int cm;
-
 //________________________________________________________________________________
 void setup()
 {
@@ -12,8 +8,6 @@ void setup()
     pinMode(13, OUTPUT); // LED
     pinMode(12, INPUT_PULLUP); // MicroSwitch 1
     pinMode(11, INPUT_PULLUP); // MicroSwitch 2
-
-    pinMode(pingPin, OUTPUT);
     
     // Debugging
     Serial.begin(9600);
@@ -22,7 +16,7 @@ void setup()
 //________________________________________________________________________________
 void loop()
 {
-    delay(5);
+    delay(50);
 }
 
 //________________________________________________________________________________
@@ -35,33 +29,6 @@ void requestEvent()
     Serial.print(tmp_sensor[0]);
     Serial.println(tmp_sensor[1]);
 
-    //ピンをOUTPUTに設定（パルス送信のため）
-    pinMode(pingPin, OUTPUT);
-    //LOWパルスを送信
-    digitalWrite(pingPin, LOW);
-    delayMicroseconds(2);  
-    //HIGHパルスを送信
-    digitalWrite(pingPin, HIGH);  
-    //5uSパルスを送信してPingSensorを起動
-    delayMicroseconds(5); 
-    digitalWrite(pingPin, LOW); 
-    
-    //入力パルスを読み取るためにデジタルピンをINPUTに変更（シグナルピンを入力に切り替え）
-    pinMode(pingPin, INPUT);   
-    
-    //入力パルスの長さを測定
-    duration = pulseIn(pingPin, HIGH);  
-
-    //パルスの長さを半分に分割
-    duration=duration/2;  
-    //cmに変換
-    cm = int(duration/29);
-
-    Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();
-
-    delay(25);
 }//end requestEvent
 
 //________________________________________________________________________________
---
+++ test/ultrasonic.ino
@@ -0,0 +1,47 @@
+const int pingPin = 2;
+const int ledPin = 13;
+unsigned long duration;
+int cm;
+
+void setup()
+{
+  pinMode(pingPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  //ピンをOUTPUTに設定（パルス送信のため）
+  pinMode(pingPin, OUTPUT);
+  //LOWパルスを送信
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);  
+  //HIGHパルスを送信
+  digitalWrite(pingPin, HIGH);  
+  //5uSパルスを送信してPingSensorを起動
+  delayMicroseconds(5); 
+  digitalWrite(pingPin, LOW); 
+  
+  //入力パルスを読み取るためにデジタルピンをINPUTに変更（シグナルピンを入力に切り替え）
+  pinMode(pingPin, INPUT);   
+  
+  //入力パルスの長さを測定
+  duration = pulseIn(pingPin, HIGH);  
+
+  //パルスの長さを半分に分割
+  duration=duration/2;  
+  //cmに変換
+  cm = int(duration/29); 
+
+  if(cm <= 50) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+}
