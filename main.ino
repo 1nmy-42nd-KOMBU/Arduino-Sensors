bbbcb6e6b07d3d25c13b407473f59ae9a521ac77
koki0517 <45210810@gse.okayama-c.ed.jp>
2022-09-29 23:57:32 +0900

    センサーの値の処理に時間がかかることが問題のようだ

--- main.ino
+++ main.ino
@@ -138,58 +138,63 @@ void microswitches()
 
 int ultrasonic_sensor()
 {
-  Serial.println("ultrasonic");
-  // establish variables for duration of the ping, and the distance result
-  // in inches and centimeters:
-  long duration, inches, cm;
-  int pingPin = 2;
-  int result = 0;
+  // Serial.println("ultrasonic");
+  // // establish variables for duration of the ping, and the distance result
+  // // in inches and centimeters:
+  // long duration, inches, cm;
+  // int pingPin = 2;
+  // int result = 0;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
+  // // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  // pinMode(pingPin, OUTPUT);
+  // digitalWrite(pingPin, LOW);
+  // delayMicroseconds(2);
+  // digitalWrite(pingPin, HIGH);
+  // delayMicroseconds(5);
+  // digitalWrite(pingPin, LOW);
 
-  // The same pin is used to read the signal from the PING))): a HIGH pulse
-  // whose duration is the time (in microseconds) from the sending of the ping
-  // to the reception of its echo off of an object.
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
+  // // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // // whose duration is the time (in microseconds) from the sending of the ping
+  // // to the reception of its echo off of an object.
+  // pinMode(pingPin, INPUT);
+  // duration = pulseIn(pingPin, HIGH);
 
-  // convert the time into a distance
-  cm = duration / 29 / 2;
+  // // convert the time into a distance
+  // cm = duration / 29 / 2;
 
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+  // Serial.print(cm);
+  // Serial.print("cm");
+  // Serial.println();
 
-  Serial.print("You should wait for 100 ms");
-  // delay(100);
-  // unsigned long time_now = millis();
-  // while(millis() < time_now + 100){
-  //   Serial.println(millis());
-  //   Serial.println(time_now);
-  // }//wait approx. [period] ms 
-  for(int i = 0; i < 25; i++) {
+  // Serial.print("You should wait for 100 ms");
+  // // delay(100);
+  // // unsigned long time_now = millis();
+  // // while(millis() < time_now + 100){
+  // //   Serial.println(millis());
+  // //   Serial.println(time_now);
+  // // }//wait approx. [period] ms 
+  // for(int i = 0; i < 25; i++) {
+  //   delayMicroseconds(1000); // 1ms
+  //   Serial.println("delay_ms");
+  // }
+  // Serial.println("done well");
+
+  // int tmp = (int)cm;
+
+  // if (tmp > 10) {
+  //   result = 11;
+  // } else {
+  //   result = 9;
+  // }
+  // Serial.print("result: ");
+  // Serial.println(result);
+  // return result;
+  for(int i = 0; i < 100; i++) {
     delayMicroseconds(1000); // 1ms
     Serial.println("delay_ms");
   }
-  Serial.println("done well");
-
-  int tmp = (int)cm;
-
-  if (tmp > 10) {
-    result = 11;
-  } else {
-    result = 9;
-  }
-  Serial.print("result: ");
-  Serial.println(result);
-  return result;
+  return 10;
 }
 
 //________________________________________________________________________________
