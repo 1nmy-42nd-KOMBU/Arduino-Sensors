1ad3ffe132cffb302d29b2dfb9086b59d491936e
Koki <89891689+koki0517@users.noreply.github.com>
2022-12-13 03:00:07 +0900

    ESP32からtof、ジャイロ、超音波センサーの値を受け取ることに成功
    メモ、シリアルモニタ
    ```
    02:58:05.000 -> Hello Boss
    02:58:05.000 -> Hello Boss
    02:58:05.046 -> timeout
    02:58:05.046 -> 0
    02:58:05.046 -> errorHello Boss
    02:58:06.540 -> 1
    02:58:06.540 -> tof; 228
    02:58:06.540 -> Gyro; 256,0
    02:58:06.587 -> ultrasonic left; 254
    02:58:06.587 -> ultrasonic right; 254
    02:58:08.073 -> Hello Boss
    02:58:08.073 -> 5
    02:58:08.073 -> errorHello Boss
    02:58:09.562 -> 9
    02:58:09.562 -> tof; 226
    02:58:09.562 -> Gyro; 351,1
    02:58:09.609 -> ultrasonic left; 46
    02:58:09.609 -> ultrasonic right; 46
    02:58:11.064 -> Hello Boss
    02:58:11.110 -> 9
    02:58:11.110 -> tof; 228
    02:58:11.110 -> Gyro; 351,1
    02:58:11.110 -> ultrasonic left; 44
    02:58:11.156 -> ultrasonic right; 44
    02:58:12.602 -> Hello Boss
    02:58:12.602 -> 9
    02:58:12.602 -> tof; 226
    02:58:12.649 -> Gyro; 351,1
    02:58:12.649 -> ultrasonic left; 44
    02:58:12.649 -> ultrasonic right; 44
    02:58:14.128 -> Hello Boss
    02:58:14.128 -> 9
    02:58:14.128 -> tof; 230
    02:58:14.174 -> Gyro; 351,1
    02:58:14.174 -> ultrasonic left; 44
    02:58:14.174 -> ultrasonic right; 46
    02:58:15.647 -> Hello Boss
    02:58:15.647 -> 9
    02:58:15.647 -> tof; 230
    02:58:15.692 -> Gyro; 351,1
    02:58:15.692 -> ultrasonic left; 44
    02:58:15.692 -> ultrasonic right; 44
    02:58:17.165 -> Hello Boss
    02:58:17.211 -> 9
    02:58:17.211 -> tof; 228
    02:58:17.211 -> Gyro; 351,1
    02:58:17.211 -> ultrasonic left; 44
    02:58:17.258 -> ultrasonic right; 44
    ```

--- test/uart.cpp
+++ test/uart.cpp
@@ -4,24 +4,48 @@ SoftwareSerial mySerial(4, 5); // RX, TX
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
+  Serial.println("Hello Boss");
 }
 void loop() {
-  char list[] = {0,1,2,3};
   Serial.println("Hello Boss");
-  mySerial.write(list,4);
-  
-  while (mySerial.available() <= 0){}
+  mySerial.write(32);
+
+  bool iftimeout = true;
+  unsigned long start_time = millis();
+  while (iftimeout){
+    if (mySerial.available() > 0){iftimeout = false;}
+    if (millis() - start_time > 10){
+      Serial.println("timeout");
+      break;  
+    }
+  }
   char byte_count = 0;
-  int8_t list_arduino[]  = {0,0,0,0};
-  Serial.print(mySerial.available());
+  int8_t list_arduino[9] = {0,0,0,0,0,0,0,0,0};
+  Serial.println(mySerial.available());
   while (mySerial.available() > 0) {
     list_arduino[byte_count] = mySerial.read();
     byte_count += 1;
   }
-  Serial.print(list_arduino[0]);
-  Serial.print(list_arduino[1]);
-  Serial.print(list_arduino[2]);
-  Serial.println(list_arduino[3]);
-    
+  if (list_arduino[0] == 64){
+    Serial.print("tof; "); // tof----------------------------
+    if (list_arduino[2] < 0){
+      Serial.println(list_arduino[1]*256+list_arduino[2]+256);
+    } else {
+      Serial.println(list_arduino[1]*256+list_arduino[2]);
+    }
+
+    Serial.print("Gyro; "); // Gyro--------------------------
+    int16_t x_tmp = list_arduino[3]*256+list_arduino[4];
+    if (x_tmp <= -1 && x_tmp >= -128){
+      Serial.print(x_tmp+256);
+    } else {Serial.print(x_tmp);}
+    Serial.print(",");
+    Serial.println(list_arduino[5]);
+
+    Serial.print("ultrasonic left; ");
+    Serial.println((list_arduino[6]+127)*2);
+    Serial.print("ultrasonic right; ");
+    Serial.println((list_arduino[7]+127)*2);
+  } else {Serial.print("error");}
   delay(1500);
 }
