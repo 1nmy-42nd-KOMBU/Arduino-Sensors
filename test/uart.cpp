3ce2e3a725821a45d13faa44ea770eb79119540d
Koki <89891689+koki0517@users.noreply.github.com>
2022-12-13 16:07:59 +0900

    受信したバイト数が9バイトに満たない場合break

--- test/uart.cpp
+++ test/uart.cpp
@@ -1,31 +1,39 @@
 #include <SoftwareSerial.h>
+#include<Wire.h>
 SoftwareSerial mySerial(4, 5); // RX, TX
 // this sample code provided by www.programmingboss.com
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(9600);
-  Serial.println("Hello Boss");
+  mySerial.begin(115200);
 }
+
 void loop() {
-  Serial.println("Hello Boss");
+  Serial.println("send to esp32");
   mySerial.write(32);
 
   bool iftimeout = true;
   unsigned long start_time = millis();
   while (iftimeout){
     if (mySerial.available() > 0){iftimeout = false;}
-    if (millis() - start_time > 10){
+    if (millis() - start_time > 10){ // timeout
       Serial.println("timeout");
       break;  
     }
   }
   char byte_count = 0;
   int8_t list_arduino[9] = {0,0,0,0,0,0,0,0,0};
-  Serial.println(mySerial.available());
+  int8_t left_byte = mySerial.available();
+  Serial.println(left_byte);
+
   while (mySerial.available() > 0) {
     list_arduino[byte_count] = mySerial.read();
     byte_count += 1;
   }
+  if (left_byte == 9){}else{
+    Serial.println("break");
+    break;
+  } // chack if there are 9byte
+
   if (list_arduino[0] == 64){
     Serial.print("tof; "); // tof----------------------------
     if (list_arduino[2] < 0){
